92d0c655249ae73540809c2a173b5cda434488b2
Dalton Pelc <daltonjpelc@gmail.com>
2013-10-08 17:39:00 -0400

    Updated the platform url
    Signed-off-by: Dalton Pelc <daltonjpelc@gmail.com>

--- src/main/java/net/technicpack/launchercore/restful/PlatformConstants.java
+++ src/main/java/net/technicpack/launchercore/restful/PlatformConstants.java
@@ -20,9 +20,9 @@
 package net.technicpack.launchercore.restful;
 
 public class PlatformConstants {
-	public static final String PLATFORM = "http://platform.sctgaming.com/";
+	public static final String PLATFORM = "http://www.technicpack.net/";
 
-	public static final String API = PLATFORM + "api/";
+	public static final String API = PLATFORM + "apiv2/";
 
 	public static final String MODPACK = API + "modpack/";
 
