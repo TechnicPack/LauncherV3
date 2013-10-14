2e27bef56d746d93204689a229d01131c7509727
Dalton Pelc <daltonjpelc@gmail.com>
2013-10-14 08:26:10 -0400

    Updated formatting on the assets pull request
    Signed-off-by: Dalton Pelc <daltonjpelc@gmail.com>

--- src/main/java/net/technicpack/launchercore/util/XMLUtils.java
+++ src/main/java/net/technicpack/launchercore/util/XMLUtils.java
@@ -1,19 +1,20 @@
 package net.technicpack.launchercore.util;
 
-import java.io.IOException;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
 public class XMLUtils {
-		static DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
-		static DocumentBuilder docBuilder;
-		
+	static DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+	static DocumentBuilder docBuilder;
+
 	public static Document getXMLFromURL(String url) {
 		Document xmlDoc = null;
-		
+
 		try {
 			docBuilder = builderFactory.newDocumentBuilder();
 			xmlDoc = docBuilder.parse(url);
@@ -27,7 +28,7 @@ public class XMLUtils {
 			System.err.println("Error accessing URL.");
 			return xmlDoc;
 		}
-        
+
 		xmlDoc.getDocumentElement().normalize();
 		return xmlDoc;
 	}
