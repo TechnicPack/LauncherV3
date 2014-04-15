9f0229f65f6defe824e97bbb88ac894d8fe1a438
CannibalVox <sdbaynham@gmail.com>
2014-04-15 17:42:54 -0500

    Move task queue out to the launcher.

--- src/main/java/net/technicpack/launchercore/install/ModpackInstaller.java
+++ src/main/java/net/technicpack/launchercore/install/ModpackInstaller.java
@@ -19,117 +19,47 @@
 
 package net.technicpack.launchercore.install;
 
-import net.technicpack.launchercore.exception.CacheDeleteException;
 import net.technicpack.launchercore.exception.PackNotAvailableOfflineException;
-import net.technicpack.launchercore.install.tasks.EnsureFileTask;
-import net.technicpack.launchercore.install.tasks.HandleVersionFileTask;
-import net.technicpack.launchercore.install.tasks.InitPackDirectoryTask;
-import net.technicpack.launchercore.install.tasks.InstallMinecraftIfNecessaryTask;
-import net.technicpack.launchercore.install.tasks.InstallModpackTask;
 import net.technicpack.launchercore.install.tasks.InstallTasksQueue;
-import net.technicpack.launchercore.install.tasks.VerifyVersionFilePresentTask;
-import net.technicpack.launchercore.modpacks.InstalledPack;
 import net.technicpack.launchercore.modpacks.ModpackModel;
-import net.technicpack.minecraftcore.LauncherDirectories;
 import net.technicpack.minecraftcore.mojang.CompleteVersion;
-import net.technicpack.launchercore.mirror.MirrorStore;
 import net.technicpack.platform.IPlatformApi;
-import net.technicpack.rest.io.Modpack;
-import net.technicpack.rest.io.PackInfo;
 import net.technicpack.launchercore.util.*;
 
-import net.technicpack.launchercore.install.verifiers.ValidZipFileVerifier;
 import net.technicpack.utilslib.Utils;
 import net.technicpack.utilslib.ZipUtils;
 import org.apache.commons.io.FileUtils;
 
-import javax.swing.JOptionPane;
-import java.awt.Component;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 
 public class ModpackInstaller {
-    private final LauncherDirectories directories;
     private final IPlatformApi platformApi;
     private final String clientId;
-	private boolean finished = false;
-    private MirrorStore mirrorStore;
 
-	public ModpackInstaller(LauncherDirectories directories, MirrorStore mirrorStore, IPlatformApi platformApi, String clientId) {
+	public ModpackInstaller(IPlatformApi platformApi, String clientId) {
 		this.clientId = clientId;
-        this.directories = directories;
-        this.mirrorStore = mirrorStore;
         this.platformApi = platformApi;
 	}
 
-	public CompleteVersion installPack(ModpackModel modpack, String build, DownloadListener listener) throws IOException {
+	public CompleteVersion installPack(InstallTasksQueue tasksQueue, ModpackModel modpack, String build) throws IOException {
         modpack.save();
-
-        InstallTasksQueue queue = new InstallTasksQueue(listener, mirrorStore);
-		queue.AddTask(new InitPackDirectoryTask(modpack));
-
-		PackInfo packInfo = modpack.getPackInfo();
-		Modpack modpackData = packInfo.getModpack(build);
-		String minecraft = modpackData.getMinecraft();
-
-		if (minecraft.startsWith("1.5")) {
-			queue.AddTask(new EnsureFileTask(new File(directories.getCacheDirectory(), "fml_libs15.zip"), new ValidZipFileVerifier(), new File(modpack.getInstalledDirectory(), "lib"), "http://mirror.technicpack.net/Technic/lib/fml/fml_libs15.zip"));
-		} else if (minecraft.startsWith("1.4")) {
-			queue.AddTask(new EnsureFileTask(new File(directories.getCacheDirectory(), "fml_libs.zip"), new ValidZipFileVerifier(), new File(modpack.getInstalledDirectory(), "lib"), "http://mirror.technicpack.net/Technic/lib/fml/fml_libs.zip"));
-		}
-
-		queue.RunAllTasks();
+        modpack.initDirectories();
 
 		Version installedVersion = modpack.getInstalledVersion();
-        boolean shouldUpdate = false;
-
         if (installedVersion == null) {
             platformApi.incrementPackInstalls(modpack.getName());
             Utils.sendTracking("installModpack", modpack.getName(), modpack.getBuild(), clientId);
-            shouldUpdate = true;
         }
 
-//		if (!shouldUpdate && !build.equals(installedVersion.getVersion())) {
-//			int result = JOptionPane.showConfirmDialog(parentComponent, "Would you like to update this pack?", "Update Found", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE);
-//
-//			if (result == JOptionPane.YES_OPTION) {
-//				shouldUpdate = true;
-//			} else {
-//				build = installedVersion.getVersion();
-//			}
-//		}
-
-		if (shouldUpdate) {
-			//If we're installing a new version of modpack, then we need to get rid of the existing version.json
-			File versionFile = new File(modpack.getBinDir(), "version.json");
-			if (versionFile.exists()) {
-				if (!versionFile.delete()) {
-					throw new CacheDeleteException(versionFile.getAbsolutePath());
-				}
-			}
-
-			queue.AddTask(new InstallModpackTask(modpack, modpackData));
-		}
-
-		queue.AddTask(new VerifyVersionFilePresentTask	(modpack, minecraft));
-	    queue.AddTask(new HandleVersionFileTask(modpack, directories));
-
-	    if ((installedVersion != null && installedVersion.isLegacy()) || shouldUpdate)
-			queue.AddTask(new InstallMinecraftIfNecessaryTask(modpack, minecraft, directories.getCacheDirectory()));
-
-        queue.RunAllTasks();
+        tasksQueue.RunAllTasks();
 
         Version versionFile = new Version(build, false);
         versionFile.save(modpack.getBinDir());
 
-        finished = true;
-        return queue.getCompleteVersion();
+        return tasksQueue.getCompleteVersion();
     }
-
-	public boolean isFinished() {
-		return finished;
-	}
 
 	public CompleteVersion prepareOfflinePack(ModpackModel modpack) throws IOException {
         modpack.initDirectories();
@@ -137,10 +67,8 @@ public class ModpackInstaller {
 		File versionFile = new File(modpack.getBinDir(), "version.json");
 		File modpackJar = new File(modpack.getBinDir(), "modpack.jar");
 
-		boolean didExtract = false;
-
 		if (modpackJar.exists()) {
-			didExtract = ZipUtils.extractFile(modpackJar, modpack.getBinDir(), "version.json");
+			ZipUtils.extractFile(modpackJar, modpack.getBinDir(), "version.json");
 		}
 
 		if (!versionFile.exists()) {
