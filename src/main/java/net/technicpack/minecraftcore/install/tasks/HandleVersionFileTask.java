0125c947d7933165abf55457d3cda878cb00f43b
CannibalVox <sdbaynham@gmail.com>
2014-08-19 17:04:14 -0500

    Always extract native files on lib install, even if cache already exists.

--- src/main/java/net/technicpack/minecraftcore/install/tasks/HandleVersionFileTask.java
+++ src/main/java/net/technicpack/minecraftcore/install/tasks/HandleVersionFileTask.java
@@ -22,17 +22,17 @@ package net.technicpack.minecraftcore.install.tasks;
 import net.technicpack.launchercore.exception.DownloadException;
 import net.technicpack.launchercore.install.ITasksQueue;
 import net.technicpack.launchercore.install.InstallTasksQueue;
+import net.technicpack.launchercore.install.LauncherDirectories;
 import net.technicpack.launchercore.install.tasks.EnsureFileTask;
 import net.technicpack.launchercore.install.tasks.IInstallTask;
+import net.technicpack.launchercore.install.verifiers.IFileVerifier;
+import net.technicpack.launchercore.install.verifiers.MD5FileVerifier;
+import net.technicpack.launchercore.install.verifiers.ValidZipFileVerifier;
 import net.technicpack.launchercore.modpacks.ModpackModel;
-import net.technicpack.launchercore.install.LauncherDirectories;
 import net.technicpack.minecraftcore.MojangUtils;
 import net.technicpack.minecraftcore.mojang.version.io.CompleteVersion;
 import net.technicpack.minecraftcore.mojang.version.io.Library;
 import net.technicpack.utilslib.OperatingSystem;
-import net.technicpack.launchercore.install.verifiers.IFileVerifier;
-import net.technicpack.launchercore.install.verifiers.MD5FileVerifier;
-import net.technicpack.launchercore.install.verifiers.ValidZipFileVerifier;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
@@ -40,7 +40,7 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 
 public class HandleVersionFileTask implements IInstallTask {
-	private final ModpackModel pack;
+    private final ModpackModel pack;
     private final LauncherDirectories directories;
     private final ITasksQueue checkLibraryQueue;
     private final ITasksQueue downloadLibraryQueue;
@@ -48,84 +48,88 @@ public class HandleVersionFileTask implements IInstallTask {
 
     private String libraryName;
 
-	public HandleVersionFileTask(ModpackModel pack, LauncherDirectories directories, ITasksQueue checkLibraryQueue, ITasksQueue downloadLibraryQueue, ITasksQueue copyLibraryQueue) {
-		this.pack = pack;
+    public HandleVersionFileTask(ModpackModel pack, LauncherDirectories directories, ITasksQueue checkLibraryQueue, ITasksQueue downloadLibraryQueue, ITasksQueue copyLibraryQueue) {
+        this.pack = pack;
         this.directories = directories;
         this.checkLibraryQueue = checkLibraryQueue;
         this.downloadLibraryQueue = downloadLibraryQueue;
         this.copyLibraryQueue = copyLibraryQueue;
-	}
+    }
 
-	@Override
-	public String getTaskDescription() {
+    @Override
+    public String getTaskDescription() {
         if (libraryName == null)
-		    return "Processing version.";
+            return "Processing version.";
         else
-            return "Verifying "+libraryName+".";
-	}
+            return "Verifying " + libraryName + ".";
+    }
 
-	@Override
-	public float getTaskProgress() {
-		return 0;
-	}
+    @Override
+    public float getTaskProgress() {
+        return 0;
+    }
 
-	@Override
-	public void runTask(InstallTasksQueue queue) throws IOException {
-		File versionFile = new File(this.pack.getBinDir(), "version.json");
-		String json = FileUtils.readFileToString(versionFile, Charset.forName("UTF-8"));
-		CompleteVersion version = MojangUtils.getGson().fromJson(json, CompleteVersion.class);
+    @Override
+    public void runTask(InstallTasksQueue queue) throws IOException {
+        File versionFile = new File(this.pack.getBinDir(), "version.json");
+        String json = FileUtils.readFileToString(versionFile, Charset.forName("UTF-8"));
+        CompleteVersion version = MojangUtils.getGson().fromJson(json, CompleteVersion.class);
 
-		if (version == null) {
-			throw new DownloadException("The version.json file was invalid.");
-		}
+        if (version == null) {
+            throw new DownloadException("The version.json file was invalid.");
+        }
 
         ValidZipFileVerifier zipVerifier = new ValidZipFileVerifier();
 
-		for (Library library : version.getLibrariesForOS()) {
-			// If minecraftforge is described in the libraries, skip it
-			// HACK - Please let us get rid of this when we move to actually hosting forge,
-			// or at least only do it if the users are sticking with modpack.jar
-			if (library.getName().startsWith("net.minecraftforge:minecraftforge") ||
-					library.getName().startsWith("net.minecraftforge:forge")) {
-				continue;
-			}
+        for (Library library : version.getLibrariesForOS()) {
+            // If minecraftforge is described in the libraries, skip it
+            // HACK - Please let us get rid of this when we move to actually hosting forge,
+            // or at least only do it if the users are sticking with modpack.jar
+            if (library.getName().startsWith("net.minecraftforge:minecraftforge") ||
+                    library.getName().startsWith("net.minecraftforge:forge")) {
+                continue;
+            }
 
-            String[] nameBits = library.getName().split(":",3);
-            libraryName = nameBits[1]+"-"+nameBits[2]+".jar";
+            String[] nameBits = library.getName().split(":", 3);
+            libraryName = nameBits[1] + "-" + nameBits[2] + ".jar";
             queue.refreshProgress();
 
-			String natives = null;
-			File extractDirectory = null;
-			if (library.getNatives() != null) {
-				natives = library.getNatives().get(OperatingSystem.getOperatingSystem());
+            String natives = null;
+            File extractDirectory = null;
+            if (library.getNatives() != null) {
+                natives = library.getNatives().get(OperatingSystem.getOperatingSystem());
 
-				if (natives != null) {
-					extractDirectory = new File(this.pack.getBinDir(), "natives");
-				}
-			}
+                if (natives != null) {
+                    extractDirectory = new File(this.pack.getBinDir(), "natives");
+                }
+            }
 
-			String path = library.getArtifactPath(natives).replace("${arch}", System.getProperty("sun.arch.data.model"));
+            String path = library.getArtifactPath(natives).replace("${arch}", System.getProperty("sun.arch.data.model"));
 
-			File cache = new File(directories.getCacheDirectory(), path);
-			if (cache.getParentFile() != null) {
-				cache.getParentFile().mkdirs();
-			}
+            File cache = new File(directories.getCacheDirectory(), path);
+            if (cache.getParentFile() != null) {
+                cache.getParentFile().mkdirs();
+            }
 
-            if (cache.exists() && zipVerifier.isFileValid(cache))
+            if (cache.exists() && zipVerifier.isFileValid(cache) && extractDirectory == null)
                 continue;
 
             IFileVerifier verifier = null;
-            String url = library.getDownloadUrl(path, queue.getMirrorStore()).replace("${arch}", System.getProperty("sun.arch.data.model"));
-            String md5 = queue.getMirrorStore().getETag(url);
-            if (md5 != null && !md5.isEmpty()) {
-                verifier = new MD5FileVerifier(md5);
-            } else {
-                verifier = zipVerifier;
+            String url = null;
+
+            if (!cache.exists() || !zipVerifier.isFileValid(cache)) {
+                url = library.getDownloadUrl(path, queue.getMirrorStore()).replace("${arch}", System.getProperty("sun.arch.data.model"));
+                String md5 = queue.getMirrorStore().getETag(url);
+                if (md5 != null && !md5.isEmpty()) {
+                    verifier = new MD5FileVerifier(md5);
+                } else {
+                    verifier = zipVerifier;
+                }
             }
 
-			checkLibraryQueue.addTask(new EnsureFileTask(cache, verifier, extractDirectory, url, downloadLibraryQueue, copyLibraryQueue));
-		}
+            checkLibraryQueue.addTask(new EnsureFileTask(cache, verifier, extractDirectory, url, downloadLibraryQueue, copyLibraryQueue));
+        }
 
-		queue.setCompleteVersion(version);
-	}
+        queue.setCompleteVersion(version);
+    }
 }
