2a8925dabb06d862d54561055804af544b06bbfa
CannibalVox <sdbaynham@gmail.com>
2014-08-20 19:36:55 -0500

    Fixed license text in all files.

--- src/main/java/net/technicpack/launcher/ui/controls/modpacks/ModpackWidget.java
+++ src/main/java/net/technicpack/launcher/ui/controls/modpacks/ModpackWidget.java
@@ -3,7 +3,7 @@
  * Copyright (C) 2013 Syndicate, LLC
  *
  * The Technic Launcher is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
@@ -12,9 +12,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License,
- * as well as a copy of the GNU Lesser General Public License,
- * along with The Technic Launcher.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with the Technic Launcher.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package net.technicpack.launcher.ui.controls.modpacks;
