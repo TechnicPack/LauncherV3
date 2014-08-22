c47566a48f4a57df8f49676af23588f1d2c51d6b
CannibalVox <sdbaynham@gmail.com>
2014-08-22 03:11:42 -0500

    Updated searchresult fields for modified protocol

--- src/main/java/net/technicpack/platform/io/SearchResult.java
+++ src/main/java/net/technicpack/platform/io/SearchResult.java
@@ -22,9 +22,11 @@ package net.technicpack.platform.io;
 public class SearchResult {
     private int id;
     private String name;
-    private String friendly_name;
+    private String slug;
+    private String url;
 
     public int getId() { return id; }
     public String getDisplayName() { return name; }
-    public String getSlug() { return friendly_name; }
+    public String getSlug() { return slug; }
+    public String getUrl() { return url; }
 }
