e4209910bc5c6c0cc4b24d5f37b09823d9f06ee4
CannibalVox <sdbaynham@gmail.com>
2014-10-05 02:22:15 -0500

    Try to fix some dropshadow weirdness in linux

--- src/main/java/net/technicpack/ui/controls/borders/DropShadowBorder.java
+++ src/main/java/net/technicpack/ui/controls/borders/DropShadowBorder.java
@@ -63,6 +63,8 @@ public class DropShadowBorder extends AbstractBorder {
 
         Graphics2D g2 = shadow.createGraphics();
         g2.setRenderingHints(hints);
+        g2.setColor(new Color(0,0,0,0));
+        g2.fillRect(0,0,width,height);
         g2.setColor(color);
         g2.fillRect(thickness * 4, thickness * 4, width - (thickness * 8), height - (thickness * 8));
         g2.dispose();
