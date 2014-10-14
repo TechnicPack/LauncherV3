55fc576010b0a24371871ad16a0e3c8dd11e805d
CannibalVox <sdbaynham@gmail.com>
2014-10-13 21:15:37 -0500

    Change tintable panel to allow a big image icon in the middle

--- src/main/java/net/technicpack/ui/controls/TintablePanel.java
+++ src/main/java/net/technicpack/ui/controls/TintablePanel.java
@@ -25,9 +25,10 @@ import java.awt.*;
 public class TintablePanel extends JPanel {
     private Color tintColor;
     private boolean tintActive;
+    private JLabel overIcon = null;
 
     public TintablePanel() {
-
+        tintColor = new Color(0,0,0,0);
     }
 
     public Color getTintColor() { return tintColor; }
@@ -35,10 +36,34 @@ public class TintablePanel extends JPanel {
         this.tintColor = color;
     }
 
+    public void setOverIcon(ImageIcon image) {
+        if (overIcon != null) {
+            remove(overIcon);
+        }
+
+        overIcon = new JLabel(image);
+        overIcon.setVisible(false);
+        add(overIcon);
+        revalidate();
+    }
+
     public boolean isTintActive() { return tintActive; }
     public void setTintActive(boolean tintActive) {
         this.tintActive = tintActive;
+        overIcon.setVisible(tintActive);
+        revalidate();
         repaint();
+    }
+
+    @Override
+    public void doLayout() {
+        super.doLayout();
+
+        if (overIcon != null) {
+            int width = overIcon.getIcon().getIconWidth();
+            int height = overIcon.getIcon().getIconHeight();
+            overIcon.setBounds(getWidth() / 2 - width / 2, getHeight() / 2 - height / 2, width, height);
+        }
     }
 
     @Override
