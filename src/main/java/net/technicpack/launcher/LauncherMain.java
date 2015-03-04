349fd371df8860c5d41318e2e586e23548189580
CannibalVox <sdbaynham@gmail.com>
2015-03-03 21:51:09 -0600

    Incorporate officialness fix from launchercore

--- src/main/java/net/technicpack/launcher/LauncherMain.java
+++ src/main/java/net/technicpack/launcher/LauncherMain.java
@@ -292,7 +292,7 @@ public class LauncherMain {
         SettingsFactory.migrateSettings(settings, packStore, directories, users, migrators);
 
         PackLoader packList = new PackLoader(directories, packStore, packInfoRepository);
-        ModpackSelector selector = new ModpackSelector(resources, packList, new SolderPackSource("http://solder.technicpack.net/api/", solder, true), solder, platform, platformSearch, iconRepo);
+        ModpackSelector selector = new ModpackSelector(resources, packList, new SolderPackSource("http://solder.technicpack.net/api/", solder), solder, platform, platformSearch, iconRepo);
         selector.setBorder(BorderFactory.createEmptyBorder());
         userModel.addAuthListener(selector);
 
