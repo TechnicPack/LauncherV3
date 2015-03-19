61d8caed414929a5c4ad9e9c5ef65b62b8b381c9
CannibalVox <sdbaynham@gmail.com>
2015-03-19 17:15:13 -0500

    Set up new task groups

--- src/main/java/net/technicpack/launcher/launch/Installer.java
+++ src/main/java/net/technicpack/launcher/launch/Installer.java
@@ -211,6 +211,7 @@ public class Installer {
         TaskGroup verifyingAssets = new TaskGroup(resources.getString("install.message.verifyassets"));
         TaskGroup installingAssets = new TaskGroup(resources.getString("install.message.installassets"));
         TaskGroup grabLibs = new TaskGroup(resources.getString("install.message.grablibraries"));
+        TaskGroup checkNonMavenLibs = new TaskGroup(resources.getString("install.message.nonmavenlibs"));
 
         queue.addTask(examineModpackData);
         queue.addTask(verifyingFiles);
@@ -220,6 +221,7 @@ public class Installer {
         queue.addTask(installVersionFile);
         queue.addTask(examineVersionFile);
         queue.addTask(grabLibs);
+        queue.addTask(checkNonMavenLibs);
         queue.addTask(installingLibs);
         queue.addTask(installingMinecraft);
         queue.addTask(examineIndex);
@@ -247,7 +249,7 @@ public class Installer {
         }
 
         checkVersionFile.addTask(new VerifyVersionFilePresentTask(modpack, minecraft, versionBuilder));
-        examineVersionFile.addTask(new HandleVersionFileTask(modpack, directories, grabLibs, installingLibs, installingLibs, versionBuilder));
+        examineVersionFile.addTask(new HandleVersionFileTask(modpack, directories, checkNonMavenLibs, grabLibs, installingLibs, installingLibs, versionBuilder));
         examineVersionFile.addTask(new EnsureAssetsIndexTask(directories.getAssetsDirectory(), installingMinecraft, examineIndex, verifyingAssets, installingAssets, installingAssets));
 
         if (doFullInstall || (installedVersion != null && installedVersion.isLegacy()))
