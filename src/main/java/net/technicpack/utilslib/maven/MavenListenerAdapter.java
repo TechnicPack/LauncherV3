121d70702c13a6a583019b56fb187e03cb465e80
CannibalVox <sdbaynham@gmail.com>
2015-03-19 19:04:08 -0500

    Make the progress bar behave better when a maven artifact has dependencies.

--- src/main/java/net/technicpack/utilslib/maven/MavenListenerAdapter.java
+++ src/main/java/net/technicpack/utilslib/maven/MavenListenerAdapter.java
@@ -1,12 +1,19 @@
 package net.technicpack.utilslib.maven;
 
 import net.technicpack.launchercore.util.DownloadListener;
+import net.technicpack.rest.io.Resource;
 import org.eclipse.aether.transfer.TransferCancelledException;
 import org.eclipse.aether.transfer.TransferEvent;
 import org.eclipse.aether.transfer.TransferListener;
+import org.eclipse.aether.transfer.TransferResource;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MavenListenerAdapter implements TransferListener {
     private DownloadListener listener;
+    private long total = 0;
+    private Map<String, Long> completedAmounts = new HashMap<String, Long>();
 
     public MavenListenerAdapter(DownloadListener listener) {
         this.listener = listener;
@@ -14,20 +21,18 @@ public class MavenListenerAdapter implements TransferListener {
 
     @Override
     public void transferInitiated(TransferEvent transferEvent) throws TransferCancelledException {
-
     }
 
     @Override
     public void transferStarted(TransferEvent transferEvent) throws TransferCancelledException {
-        listener.stateChanged("", 0);
+        completedAmounts.put(transferEvent.getResource().getResourceName(), 0L);
+        total += transferEvent.getResource().getContentLength();
+        updateValue(transferEvent.getResource(), transferEvent.getTransferredBytes());
     }
 
     @Override
     public void transferProgressed(TransferEvent transferEvent) throws TransferCancelledException {
-        float total = transferEvent.getResource().getContentLength();
-        float progress = transferEvent.getTransferredBytes();
-        float percent = (100.0f*progress)/total;
-        listener.stateChanged("", percent);
+        updateValue(transferEvent.getResource(), transferEvent.getTransferredBytes());
     }
 
     @Override
@@ -36,11 +41,25 @@ public class MavenListenerAdapter implements TransferListener {
 
     @Override
     public void transferSucceeded(TransferEvent transferEvent) {
-        listener.stateChanged("", 100.0f);
+        updateValue(transferEvent.getResource(), transferEvent.getResource().getContentLength());
     }
 
     @Override
     public void transferFailed(TransferEvent transferEvent) {
 
+    }
+
+    private void updateValue(TransferResource resource, long bytes) {
+        completedAmounts.put(resource.getResourceName(), bytes);
+
+        long completed = 0;
+        for (Long value : completedAmounts.values()) {
+            completed += value;
+        }
+
+        float total = this.total;
+        float progress = completed;
+        float percent = (100.0f*progress)/total;
+        listener.stateChanged("", percent);
     }
 }
