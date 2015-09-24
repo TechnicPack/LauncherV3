178dbf685ac9b518fa7ae72458bd634c04b34063
CannibalVox <sdbaynham@gmail.com>
2015-09-24 09:17:36 -0400

    People are having some trouble with OOM errors and I think my tinkering is at fault.

--- src/main/java/net/technicpack/minecraftcore/launch/MinecraftLauncher.java
+++ src/main/java/net/technicpack/minecraftcore/launch/MinecraftLauncher.java
@@ -109,18 +109,13 @@ public class MinecraftLauncher {
         // So in 1.8 permgen autoscales- only problem, it doesn't do it based on RAM allocation like we do, instead
         // It has a SEPARATE heap for permgen that is by default unbounded by anything.  Result: instead of 2GB
         // with 256m set aside for permgen, you have a whole 2GB PLUS however much permgen uses.
-
-        //So to deal with this, we're going to reduce the memory by the permgen size and bound the metaspace by it to
-        //make sure we use the specified amount as expected.
-        if (RunData.isJavaVersionAtLeast(launchJavaVersion, "1.8")) {
-            memory -= permSize;
-        }
+        
         commands.add("-Xmx" + memory + "m");
 
         if (!RunData.isJavaVersionAtLeast(launchJavaVersion, "1.8"))
             commands.add("-XX:MaxPermSize=" + permSize + "m");
         else
-            commands.add("-XX:MetaspaceSize=" + permSize + "m");
+            commands.add("-XX:MetaspaceSize=" + permSize*2 + "m");
 
         if (RunData.isJavaVersionAtLeast(launchJavaVersion, "1.7")) {
             commands.add("-XX:+UseG1GC");
