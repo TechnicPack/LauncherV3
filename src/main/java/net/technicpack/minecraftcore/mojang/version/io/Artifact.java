e67dff593bf126c2e4d8b818db143e789e926f87
Pedro Cunha <pedroagracio@gmail.com>
2020-09-06 17:21:40 +0100

    Remove unused path from library artifacts

--- src/main/java/net/technicpack/minecraftcore/mojang/version/io/Artifact.java
+++ src/main/java/net/technicpack/minecraftcore/mojang/version/io/Artifact.java
@@ -2,14 +2,9 @@ package net.technicpack.minecraftcore.mojang.version.io;
 
 @SuppressWarnings({"unused"})
 public class Artifact {
-    private String path;
     private String url;
     private String sha1;
     private Integer size;
-
-    public String getPath() {
-        return path;
-    }
 
     public String getUrl() {
         return url;
@@ -22,7 +17,4 @@ public class Artifact {
     public Integer getSize() {
         return size;
     }
-
-
-
 }
