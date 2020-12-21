858a816f4fbb21be6192fef4b50930af221312ed
balika011 <balika011@gmail.com>
2020-12-21 08:24:46 +0100

    workaround for java 8

--- src/main/java/net/technicpack/minecraftcore/live/auth/XboxSignature.java
+++ src/main/java/net/technicpack/minecraftcore/live/auth/XboxSignature.java
@@ -1,5 +1,9 @@
 package net.technicpack.minecraftcore.live.auth;
 
+import sun.security.util.DerInputStream;
+import sun.security.util.DerValue;
+
+import java.math.BigInteger;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.ByteBuffer;
@@ -29,6 +33,42 @@ public class XboxSignature {
         }
     }
 
+    private byte[] decodeSignature(byte[] signature) throws SignatureException {
+        try {
+            DerInputStream in = new DerInputStream(signature);
+            DerValue[] values = in.getSequence(2);
+            BigInteger r = values[0].getPositiveBigInteger();
+            BigInteger s = values[1].getPositiveBigInteger();
+            // trim leading zeroes
+            byte[] rBytes = trimZeroes(r.toByteArray());
+            byte[] sBytes = trimZeroes(s.toByteArray());
+            int k = Math.max(rBytes.length, sBytes.length);
+            // r and s each occupy half the array
+            byte[] result = new byte[k << 1];
+            System.arraycopy(rBytes, 0, result, k - rBytes.length,
+                    rBytes.length);
+            System.arraycopy(sBytes, 0, result, result.length - sBytes.length,
+                    sBytes.length);
+            return result;
+
+        } catch (Exception e) {
+            throw new SignatureException("Could not decode signature", e);
+        }
+    }
+
+    private static byte[] trimZeroes(byte[] b) {
+        int i = 0;
+        while ((i < b.length - 1) && (b[i] == 0)) {
+            i++;
+        }
+        if (i == 0) {
+            return b;
+        }
+        byte[] t = new byte[b.length - i];
+        System.arraycopy(b, i, t, 0, t.length);
+        return t;
+    }
+
     public String SignRequest(String method, String url, Map<String, String> headers, String data) {
         try {
             ByteBuffer buffer = ByteBuffer.allocate(8);
@@ -37,7 +77,7 @@ public class XboxSignature {
             buffer.flip();
             byte[] time = buffer.array();
 
-            Signature ecdsaSign = Signature.getInstance("SHA256withECDSAinP1363format");
+            Signature ecdsaSign = Signature.getInstance("SHA256withECDSA");
             ecdsaSign.initSign(privateKey);
 
             byte[] policyVersion =  {0, 0, 0, 1};
@@ -64,7 +104,7 @@ public class XboxSignature {
             ecdsaSign.update(data.getBytes());
             ecdsaSign.update((byte) 0);
 
-            byte[] signature = ecdsaSign.sign();
+            byte[] signature = decodeSignature(ecdsaSign.sign());
 
             ByteBuffer out = ByteBuffer.allocate(policyVersion.length + time.length + signature.length);
             out.order(ByteOrder.LITTLE_ENDIAN);
