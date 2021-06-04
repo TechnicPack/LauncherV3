562d89913cbdc00007b88ffbfc30b157357b436e
Pedro Cunha <pedroagracio@gmail.com>
2021-06-05 00:53:09 +0100

    Drop IOException from AuthenticationException

--- src/main/java/net/technicpack/launchercore/exception/AuthenticationException.java
+++ src/main/java/net/technicpack/launchercore/exception/AuthenticationException.java
@@ -21,7 +21,7 @@ package net.technicpack.launchercore.exception;
 
 import java.io.IOException;
 
-public class AuthenticationException extends IOException {
+public class AuthenticationException extends Exception {
     private static final long serialVersionUID = 5887385045789342851L;
 
     public AuthenticationException(String message) {
