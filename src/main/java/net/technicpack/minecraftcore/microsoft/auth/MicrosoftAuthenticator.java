cf4e9d593d395c03e98718d76682e26b15ec2d19
Pedro Cunha <pedroagracio@gmail.com>
2021-06-05 00:54:54 +0100

    Fix NPE with XSTS 401 response

--- src/main/java/net/technicpack/minecraftcore/microsoft/auth/MicrosoftAuthenticator.java
+++ src/main/java/net/technicpack/minecraftcore/microsoft/auth/MicrosoftAuthenticator.java
@@ -18,6 +18,7 @@ import net.technicpack.utilslib.Utils;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.UUID;
@@ -185,6 +186,13 @@ public class MicrosoftAuthenticator {
             httpResponse.setLoggingEnabled(true);
 
             if (httpResponse.getStatusCode() == HttpStatusCodes.STATUS_CODE_UNAUTHORIZED) {
+                // This is weird but if we don't do this then parseAs will throw an NPE because the response body is null
+                try (InputStream httpResponseContent = httpResponse.getContent()) {
+                    if (httpResponseContent == null) {
+                        throw new MicrosoftAuthException(NO_XBOX_ACCOUNT, "No Xbox account");
+                    }
+                }
+
                 XSTSUnauthorized unauthorized = httpResponse.parseAs(XSTSUnauthorized.class);
                 throw new MicrosoftAuthException(unauthorized.getExceptionType(), "Failed to get XSTS authentication.");
             } else if (httpResponse.getStatusCode() == HttpStatusCodes.STATUS_CODE_OK) {
