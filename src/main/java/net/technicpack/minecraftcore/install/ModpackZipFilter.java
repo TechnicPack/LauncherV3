eef21dcd62fa50123722650fd70bdce58deac18f
Pedro Cunha <pedroagracio@gmail.com>
2022-10-26 23:23:09 +0100

    Tighten restrictions on reserved/restricted modpack paths

--- src/main/java/net/technicpack/minecraftcore/install/ModpackZipFilter.java
+++ src/main/java/net/technicpack/minecraftcore/install/ModpackZipFilter.java
@@ -6,18 +6,25 @@ import net.technicpack.utilslib.Utils;
 
 import java.io.File;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.logging.Level;
+import java.util.stream.Stream;
 
 public class ModpackZipFilter implements IZipFileFilter {
     private final ModpackModel pack;
-    private final Path binVersionPath;
     private final Path cacheDirPath;
+    private final Path binPath;
+    private final ArrayList<Path> allowedInBinDir;
 
     public ModpackZipFilter(ModpackModel pack) {
         this.pack = pack;
 
-        this.binVersionPath = this.pack.getBinDir().toPath().resolve("version");
         this.cacheDirPath = this.pack.getCacheDir().toPath();
+        this.binPath = this.pack.getBinDir().toPath();
+
+        // Only some files in bin/ are allowed: modpack.jar, version.json, runData
+        allowedInBinDir = new ArrayList<>();
+        Stream.of("modpack.jar", "version.json", "runData").forEach(filename -> allowedInBinDir.add(this.binPath.resolve(filename)));
     }
 
     private void warnAboutFile(File file) {
@@ -26,20 +33,21 @@ public class ModpackZipFilter implements IZipFileFilter {
 
     @Override
     public boolean shouldExtract(String fileName) {
-        /*
-        Prevent some special modpack-related files from being replaced when extracting modpack/mods
-        These files are:
-        - bin/version, used by the launcher to keep track of the currently installed modpack version
-        - the cache folder and anything inside of it, which the launcher uses as storage for the modpack mods
-         */
+        // Prevent some special modpack-related files from being replaced when extracting the modpack:
+        // - Anything in the bin/ folder, besides modpack.jar, version.json and runData
+        //     - modpack.jar is used to inject things into the Minecraft jar
+        //     - version.json is usually not used, but Fabric uses this to distribute itself
+        //     - runData is used to specify the minimum RAM and Java version for non-Solder modpacks
+        //     - anything else is removed for compatibility issues between different OSs and systems
+        // - The cache/ folder and anything inside it, which the launcher uses as storage for the modpack mods
         File file = new File(this.pack.getInstalledDirectory(), fileName);
 
         // We use a Path here so we can properly compare paths instead of Java trying to compare them as strings,
         // which would match stuff like "cachefoo" in the "cache" dir check
         Path path = file.toPath().normalize();
 
-        // Check if file is $MODPACK/bin/version
-        if (path.equals(this.binVersionPath)) {
+        // Check if file is in $MODPACK/bin/ and not one of the allowed files
+        if (path.startsWith(binPath) && !path.equals(binPath) && !allowedInBinDir.contains(path)) {
             warnAboutFile(file);
             return false;
         }
